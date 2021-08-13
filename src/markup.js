b31a935b01ad4ae061506cb2c061658fb4a9d8af
Iliya Lunev <lunevig90@gmail.com>
2021-08-13 14:28:36 +0300

    update if

--- src/markup.js
+++ src/markup.js
@@ -1,4 +1,4 @@
-import countryCard from '../templates/countrys.hbs';
+import countryCard from '../src/templates/countrys.hbs';
 
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/desktop/dist/PNotifyDesktop';
@@ -10,13 +10,15 @@ const ref = { containerCountry: document.querySelector('.js-countries') };
 function markup(data) {
   const markupCard = countryCard(data);
 
-  if (data.length >= 3) {
+  if (data.length > 10) {
     error({
       title: `Too many matches found.`,
       text: `We found ${data.length} countries. Please enter a more specific query!`,
       styling: 'brighttheme',
-      delay: 500,
+      delay: 1000,
     });
+  }
+  if (data.length >= 2 && data.length <= 10) {
     return data.forEach(
       country => (ref.containerCountry.innerHTML += `<li>${country.name}</li>`),
     );
